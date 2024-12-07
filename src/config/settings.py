0d6aa1fec077a410a85881a0b1e2012ef4f445f2
Vladislav <vladislavrumyantsev@gmail.com>
2024-12-07 18:18:00 +0300

    added apps

--- src/config/settings.py
+++ src/config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.realty.apps.RealtyConfig',
+    'apps.developers.apps.DevelopersConfig',
 ]
 
 MIDDLEWARE = [

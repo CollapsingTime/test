0cc60b6f6d69905d6ed4d3bf726cacbffd34be1f
Vladislav <vladislavrumyantsev@gmail.com>
2024-12-08 11:38:22 +0300

    Created tests

--- src/apps/realty/tests.py
+++ src/apps/realty/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+import pytest
+from apps.realty.models import Flat
+from apps.developers.models import Developer
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_dev_model():
+    title = "test dev"
+    developer = Developer.objects.create(
+            title = title
+        )
+    
+    assert isinstance(developer, Developer)
+    assert developer.title == title
+
+@pytest.mark.django_db
+def test_flat_model():
+    article = "test article"
+    area = 10
+    price = 5
+
+    developer = Developer.objects.create(
+            title = "Test developer"
+        )
+
+    flat = Flat.objects.create(
+        article=article,
+        area=area,
+        price=price,
+        developer=developer
+    )
+
+    assert isinstance(flat, Flat)
+    assert flat.article == article
+    assert flat.area == area
+    assert flat.price == price
+    assert flat.developer == developer
